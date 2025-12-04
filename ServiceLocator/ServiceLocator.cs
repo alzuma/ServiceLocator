fa5cc9b19a2e720992a9423adf37dd1e2b13452e
Kristaps Vitolins <kristaps.vitolins@devlead.lv>
2025-12-04 10:40:42 +0200

    feat(core): implement keyed service registration
    - add two-phase registration: non-keyed via Scrutor, keyed via reflection
    - register keyed services using AddKeyed* methods
    - keyed services registered as self and interfaces
    
    🔒 - Generated by DACE

--- ServiceLocator/ServiceLocator.cs
+++ ServiceLocator/ServiceLocator.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
 namespace ServiceLocator
@@ -7,23 +10,79 @@ namespace ServiceLocator
     {
         public static IServiceCollection AddServiceLocator<T>(this IServiceCollection services)
         {
+            // Phase 1: Non-keyed services using Scrutor (existing logic)
             var scanServices = new ServiceCollection();
-
             scanServices.Scan(scan =>
                 scan.FromAssemblyOf<T>()
-                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Scoped))
+                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Scoped && s.Key == null))
                     .AsSelfWithInterfaces()
                     .WithScopedLifetime()
-                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Singleton))
+                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Singleton && s.Key == null))
                     .AsSelfWithInterfaces()
                     .WithSingletonLifetime()
-                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Transient))
+                    .AddClasses(classes => classes.WithAttribute<Service>(s => s.Lifetime == ServiceLifetime.Transient && s.Key == null))
                     .AsSelfWithInterfaces()
                     .WithTransientLifetime()
             );
 
-            services.Add(scanServices);
+            foreach (var service in scanServices)
+            {
+                services.Add(service);
+            }
+
+            // Phase 2: Keyed services using manual reflection
+            var assembly = typeof(T).Assembly;
+            var keyedServiceTypes = assembly.GetTypes()
+                .Where(type => type.IsClass && !type.IsAbstract)
+                .Select(type => new
+                {
+                    Type = type,
+                    Attributes = type.GetCustomAttributes<Service>(false).ToArray()
+                })
+                .Where(x => x.Attributes.Length > 0)
+                .SelectMany(x => x.Attributes.Select(attr => new { x.Type, Attribute = attr }))
+                .Where(x => x.Attribute.Key != null);
+
+            foreach (var serviceInfo in keyedServiceTypes)
+            {
+                var serviceType = serviceInfo.Type;
+                var attribute = serviceInfo.Attribute;
+                var interfaces = serviceType.GetInterfaces();
+
+                // Register as self
+                RegisterKeyedService(services, serviceType, serviceType, attribute.Key!, attribute.Lifetime);
+
+                // Register as interfaces
+                foreach (var interfaceType in interfaces)
+                {
+                    RegisterKeyedService(services, interfaceType, serviceType, attribute.Key!, attribute.Lifetime);
+                }
+            }
+
             return services;
+        }
+
+        private static void RegisterKeyedService(
+            IServiceCollection services,
+            Type serviceType,
+            Type implementationType,
+            object serviceKey,
+            ServiceLifetime lifetime)
+        {
+            switch (lifetime)
+            {
+                case ServiceLifetime.Scoped:
+                    services.AddKeyedScoped(serviceType, serviceKey, implementationType);
+                    break;
+                case ServiceLifetime.Singleton:
+                    services.AddKeyedSingleton(serviceType, serviceKey, implementationType);
+                    break;
+                case ServiceLifetime.Transient:
+                    services.AddKeyedTransient(serviceType, serviceKey, implementationType);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, "Invalid service lifetime");
+            }
         }
     }
 }
